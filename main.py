b61b0f2d7d151477e6472eab065a9bcde94bc101
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-10-22 19:49:05 -0400

    Rudimentary menu & screen manager

---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+main.py
+
+The central manager for our study app.
+
+History: 
+22 Oct 2025 - Created, add terrible mock menu
+'''
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+from screens.editscreen import EditScreen
+
+class MenuScreen(Screen):
+    '''
+    Default screen that is loaded on instantiating StudyApp
+    '''
+    screens = ['editSet']
+
+class StudyApp(App):
+    '''
+    StudyApp kivy.app inherited class
+    '''
+    def build(self): 
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(EditScreen(name='editSet'))
+
+        return sm
+
+if __name__ == "__main__":
+    StudyApp().run()
