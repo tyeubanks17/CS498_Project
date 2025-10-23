828c7902748f17ada96650caebcbb4347716b428
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-10-23 14:01:04 -0400

    Edit screen v0.1

--- screens/editscreen.py
+++ screens/editscreen.py
@@ -8,7 +8,39 @@ History:
 '''
 
 from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 from kivy.lang import Builder
+
+class TermWidget(GridLayout):
+    '''
+    Widget that will contain term & definition
+    text fields, as well as other entry manipulation
+    controls
+    '''
+    # idStr = StringProperty(None)
+    pass
+    # def __init__(self, idStr=None, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     if not self.id: 
+    #         if not idStr: 
+    #             raise ValueError("Must instantiate TermWidget with id!")
+    #         self.id = idStr
 
 class EditScreen(Screen):
     Builder.load_file("./screens/editscreen.kv")
+    terms = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add initial term objects on load
+        self.terms.append(TermWidget())
+        self.terms.append(TermWidget())
+        self.terms.append(TermWidget())
+        for tw in self.terms: 
+            self.ids['termlayout'].add_widget(tw)
+
+    def on_pre_enter(self):
+        # To execute on screen load
+        pass
